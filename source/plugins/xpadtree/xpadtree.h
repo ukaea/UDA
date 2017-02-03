8efacd9a871b51a247f3245b1cde9ff35aefaaf5
Kogan, Lucy <lucy.kogan@ccfe.ac.uk>
2017-02-03 09:25:54 +0000

    Actually adding xpadtree plugin

---
+++ source/plugins/xpadtree/xpadtree.h
@@ -0,0 +1,15 @@
+/*
+ * xpadtree.h
+ *
+ *  Created on: 24 Feb 2016
+ *      Author: lkogan
+ */
+
+#ifndef XPADTREE_H_
+#define XPADTREE_H_
+
+#include <server/idamPluginStructs.h>
+
+int idamXpadTree(IDAM_PLUGIN_INTERFACE *idam_plugin_interface);
+
+#endif /* XPADTREE_H_ */
