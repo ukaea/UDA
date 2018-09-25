63ff0d5ba8d713b251fa7a7de483f7e400e73f60
LF218007 <ludovic.fleury@cea.fr>
2018-09-25 09:41:42 +0200

    Fix mapping of pf_active.c

--- source/plugins/west/west_pf_active.c
+++ source/plugins/west/west_pf_active.c
@@ -171,8 +171,7 @@ void pf_active_elements_shapeOf(int shotNumber, DATA_BLOCK* data_block, int* nod
 
 void pf_active_coils_shapeOf(int shotNumber, DATA_BLOCK* data_block, int* nodeIndices)
 {
-	int len = 0;
-	getExtractionsCount("GPOLO_IMES", shotNumber, 0, &len);
+	int len = 17;
 	setReturnDataIntScalar(data_block, len, NULL);
 }
 
