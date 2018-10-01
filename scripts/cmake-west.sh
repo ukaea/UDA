55e8004595a0d257aa2f8e1bf0f8859bfa1ee2ae
Patrick Maini <Patrick.MAINI@cea.fr>
2018-10-01 09:01:41 +0200

    WEST Production server change

--- scripts/cmake-west.sh
+++ scripts/cmake-west.sh
@@ -7,9 +7,10 @@ module use /Applications/Modules/compilers
 module load gcc/6.4.0
 module load mdsplus/7.7-4 
 module load cmake/3.7.2 
-
+export BOOST_ROOT=/Applications/boost_1_66_0
+cmake -Bbuild -H. -DTARGET_TYPE=OTHER -DCMAKE_INSTALL_PREFIX=. \
+    -DBUILD_PLUGINS=help $* 
 cmake -Bbuild -H. -DTARGET_TYPE=OTHER -DCMAKE_INSTALL_PREFIX=. \
     -DLIBTS_ROOT=/Home/devarc/PortageMatlab7/tslib_client2013 \
-    -DBUILD_PLUGINS=imas\;help\;west\;tore_supra $* 
-
+    -DBUILD_PLUGINS=imas\;help\;west $*
 #cmake -Bbuild -H. -DTARGET_TYPE=OTHER -DCMAKE_INSTALL_PREFIX=. -DLIBTS_ROOT=/Home/devarc/PortageMatlab7/tslib_client2013
