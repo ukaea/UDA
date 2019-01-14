a4f0af5a4290f52ef17f1e366e15b043338f7697
Fleury <ludovic.fleury@cea.fr>
2019-01-14 10:28:31 +0100

    Adding gcc module to cmake script

--- scripts/cmake-tuto.sh
+++ scripts/cmake-tuto.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 module purge
 module load cineca
-module load mdsplus/7.46-1 
 module load cmake/3.5.2
+module load itm-gcc/6.1.0
+module load mdsplus/7.46-1
 cmake -Bbuild -H. -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER -DCMAKE_INSTALL_PREFIX=. \
     -DBUILD_PLUGINS=help $* 
 
