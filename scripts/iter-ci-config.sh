f621fb76291541f69c193a271b4a1c9a52e638c0
Simon Pinches <Simon.Pinches@iter.org>
2019-02-04 14:59:45 +0100

    More tweaks to cmake configure in ITER CI environment

--- scripts/iter-ci-config.sh
+++ scripts/iter-ci-config.sh
@@ -5,9 +5,7 @@
 # Set up environment for compilation
 . scripts/iter-ci-setup-env.sh || exit 1
 
-export HDF5_ROOT=$HDF5_DIR
-
 cmake -Bbuild -H. \
     -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER -DBOOST_ROOT=${EBROOTBOOST} \
-    -DPostgreSQL_ROOT=${EBROOTPOSTGRESQL} -DNETCDF_DIR=${EBROOTNETCDF}       \
+    -DHDF5_ROOT=${EBROOTHDF5} -DPostgreSQL_ROOT=${EBROOTPOSTGRESQL} -DNETCDF_DIR=${EBROOTNETCDF}       \
     -DCMAKE_INSTALL_PREFIX=. -DITER_CI=ON
