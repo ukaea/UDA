3bb347969fbb9c8d3a7353aa4c742380ca065739
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2019-04-05 11:52:05 +0200

    fixing rename of itm script and fixing flags

--- scripts/cmake-itm-marconi.sh
+++ scripts/cmake-itm-marconi.sh
@@ -7,14 +7,15 @@ module ()
 
 module purge
 module load cineca
-module load imasenv
-module switch mdsplus/alpha
+module load itmenv
+#module load boost/1.61.0--intelmpi--2017--binary
 
 export CC=gcc
 export CXX=g++
-export BOOST_ROOT=$HOME/itmwork/boost_1_62_0
+export BOOST_ROOT=$HOME/boost_1_62_0
 
 cmake -Bbuild -H. -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER \
     -DNO_MODULES=ON \
     -DSWIG_EXECUTABLE:FILEPATH=/afs/eufus.eu/user/g/g2jhollo/bin/swig \
-    -DCMAKE_INSTALL_PREFIX=$HOME/uda -DBUILD_PLUGINS=help\;imas $*
+    -DCMAKE_INSTALL_PREFIX=/gw/swimas/extra/uda/develop \
+    -DBUILD_PLUGINS=help\;uda $*
