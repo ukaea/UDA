07368be85c521ca25fc09a8514289571a741cb9a
Hervé Ancher <herve.ancher@cea.fr>
2019-08-02 16:14:15 +0200

    Fix NSIS package fabrication for Windows/MinGW

--- scripts/iter-ci-config.sh
+++ scripts/iter-ci-config.sh
@@ -5,7 +5,20 @@
 # Set up environment for compilation
 . scripts/iter-ci-setup-env.sh || exit 1
 
-cmake -Bbuild -H. \
-    -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER -DBOOST_ROOT=${EBROOTBOOST} \
-    -DHDF5_ROOT=${EBROOTHDF5} -DPostgreSQL_ROOT=${EBROOTPOSTGRESQL} -DNETCDF_DIR=${EBROOTNETCDF}       \
-    -DCMAKE_INSTALL_PREFIX=. -DITER_CI=ON
+if [ "$OS" == "Windows_NT" ] && [ "$MSYSTEM" != "" ]
+then
+	echo "MSYS2/MinGW environnement under Windows"
+	
+	cmake -Bbuild -H. -G"Unix Makefiles" \
+		-DHDF5_ROOT=${HDF5_ROOT} -DPostgreSQL_ROOT=${PostgresSQL_ROOT} -DNETCDF_DIR=${NETCDF_DIR} \
+		-DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER \
+		-DBUILD_SHARED_LIBS=ON -DNO_MODULES=ON \
+		-DCMAKE_INSTALL_PREFIX=. -DITER_CI=ON
+else
+	echo "Linux environnement"
+	
+	cmake -Bbuild -H. \
+		-DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER -DBOOST_ROOT=${EBROOTBOOST} \
+		-DHDF5_ROOT=${EBROOTHDF5} -DPostgreSQL_ROOT=${EBROOTPOSTGRESQL} -DNETCDF_DIR=${EBROOTNETCDF} \
+		-DCMAKE_INSTALL_PREFIX=. -DITER_CI=ON
+fi
--- scripts/iter-ci-setup-env.sh
+++ scripts/iter-ci-setup-env.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 # Bamboo Build script
-# Stage 0 : load modules
 
-# Set up environment for compilation
-. /usr/share/Modules/init/sh
-module use /work/imas/etc/modulefiles
-module use /work/imas/etc/modules/all
-module purge
+if [ "$OS" == "Windows_NT" ] && [ "$MSYSTEM" != "" ]
+then
+	echo "MSYS2/MinGW environnement under Windows"
+	
+	# Convert Windows style path to Linux
+	export JAVA_HOME=`cygpath.exe -u "$JAVA_HOME"`
+	export JAVA_INCLUDE_PATH=$JAVA_HOME/include
+	export JAVA_INCLUDE_PATH2=$JAVA_HOME/include/win32
+	export JAVA_AWT_INCLUDE_PATH=$JAVA_INCLUDE_PATH
+	export JNI_INCLUDE_DIR=$JAVA_INCLUDE_PATH
+	export JNI_MD_INCLUDE_DIR=$JAVA_INCLUDE_PATH/win32
+	export JNI_LIB_DIR=$AVA_HOME/lib
+	export JAVA_AWT_LIBRARY=$NI_LIB_DIR/jawt.lib
+	export JAVA_JVM_LIBRARY=$JNI_LIB_DIR/jvm.lib
+	export PATH=$AVA_HOME/bin:$PATH
+	
+	export HDF5_ROOT=`cygpath.exe -u "$HDF5_ROOT"`
+	export PostgresSQL_ROOT=`cygpath.exe -u "$PostgresSQL_ROOT"`
+	export NETCDF_DIR=`cygpath.exe -u "$NETCDF_DIR"`
+else
+	echo "Linux environnement"
+	# Stage 0 : load modules
 
-# FOSS environment based upon GCC v6.4.0
+	# Set up environment for compilation
+	. /usr/share/Modules/init/sh
+	module use /work/imas/etc/modulefiles
+	module use /work/imas/etc/modules/all
+	module purge
 
-module load CMake/3.12.1-GCCcore-6.4.0
+	# FOSS environment based upon GCC v6.4.0
 
-module load PostgreSQL/10.3-foss-2018a-Python-2.7.14
-module load libMemcached/1.0.18-GCCcore-6.4.0
-module load SWIG/3.0.12-foss-2018a-Python-2.7.14
-module load Boost/1.66.0-foss-2018a
-module load netCDF/4.6.0-foss-2018a
-module load HDF5/1.10.1-foss-2018a
-module load MDSplus-Java/7.49.1-GCCcore-6.4.0-Java-1.8.0_162
+	module load CMake/3.12.1-GCCcore-6.4.0
+
+	module load PostgreSQL/10.3-foss-2018a-Python-2.7.14
+	module load libMemcached/1.0.18-GCCcore-6.4.0
+	module load SWIG/3.0.12-foss-2018a-Python-2.7.14
+	module load Boost/1.66.0-foss-2018a
+	module load netCDF/4.6.0-foss-2018a
+	module load HDF5/1.10.1-foss-2018a
+	module load MDSplus-Java/7.49.1-GCCcore-6.4.0-Java-1.8.0_162	
+fi
