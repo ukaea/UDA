0bd6658e5d35f1c5d0c603679b6cbfa3ac064c47
Hervé Ancher <herve.ancher@cea.fr>
2019-08-26 15:20:16 +0200

    Add echo for debugging

--- scripts/iter-ci-config.sh
+++ scripts/iter-ci-config.sh
@@ -9,6 +9,11 @@ if [ "$OS" == "Windows_NT" ]
 then
 	echo "MSYS2/MinGW environnement under Windows"
 	
+	echo "JAVA_HOME: $JAVA_HOME"
+	echo "HDF5_ROOT: $HDF5_ROOT"
+	echo "PostgresSQL_ROOT: $PostgresSQL_ROOT"
+	echo "NETCDF_DIR: $NETCDF_DIR"
+	
 	cmake -Bbuild -H. -G"Unix Makefiles" \
 		-DHDF5_ROOT=${HDF5_ROOT} -DPostgreSQL_ROOT=${PostgresSQL_ROOT} -DNETCDF_DIR=${NETCDF_DIR} \
 		-DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=OTHER \
