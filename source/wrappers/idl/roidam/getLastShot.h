ff8587b08070edc36c1dd633d96f1dc312504b5b
Kogan, Lucy <lucy.kogan@ukaea.uk>
2019-11-14 14:33:49 +0000

    Moving MAST IDL wrappers out of main UDA repo into mastcodes and mastucpf repos

--- source/wrappers/idl/roidam/getLastShot.h
+++
@@ -1,8 +0,0 @@
-#ifndef UDA_WRAPPERS_IDL_GETLASTSHOT_H
-#define UDA_WRAPPERS_IDL_GETLASTSHOT_H
-
-#include <libpq-fe.h>
-
-int getLastShot(PGconn *DBConnect, int *exp_number, int verbose, int debug);
-
-#endif // UDA_WRAPPERS_IDL_GETLASTSHOT_H
