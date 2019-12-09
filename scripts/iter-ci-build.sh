cfc278c31f0f3bbf049bb437a58c8c1863f9a138
Hervé Ancher <herve.ancher@cea.fr>
2019-12-09 09:19:16 +0100

    Generate portable XDR library for Windows platform

--- scripts/iter-ci-build.sh
+++ scripts/iter-ci-build.sh
@@ -5,4 +5,21 @@
 # Set up environment for compilation
 source ./scripts/iter-ci-setup-env.sh || exit 1
 
+if [ "$OS" == "Windows_NT" ]
+then
+	# Make portable XDR
+	cd extlib/portablexdr-4.9.1
+	autoconf
+	./configure
+	make
+	
+	# Deploy portable XDR libraries
+	cd ..
+	./install.sh
+	
+	# Return to UDA level
+	cd ..
+fi
+
+# Make UDA
 make -C build
