85bfb99ce3b4778c14b48612c9748a354f921ec3
Jonathan Hollocombe <jonathan.hollocombe@ccfe.ac.uk>
2021-10-05 11:40:46 +0100

    updating cmake files for freia

--- scripts/cmake-freia.sh
+++ scripts/cmake-freia.sh
@@ -2,20 +2,14 @@
 
 module purge
 module load idl/08.3
-module load mdsplus/6.1
 module load java/1.8
-module load ida
 module load hdf5-C/1.8.13
-module load netcdf-C/4.3.2
 
 export HDF5_USE_SHLIB=yes
 export BOOST_ROOT=/usr/local/depot/boost-1.60
-export CPLUS_INCLUDE_PATH=/usr/include/libxml2:$CPLUS_INCLUDE_PATH
-export C_INCLUDE_PATH=/usr/include/libxml2:$C_INCLUDE_PATH
 
 CC=gcc CXX=g++ cmake3 -Bbuild_freia -H. \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=/projects/UDA/uda-install-develop \
-    -DLIBMEMCACHED_ROOT=/common/projects/UDA/libmemcached \
     -DBUILD_SHARED_LIBS=ON \
     $*
