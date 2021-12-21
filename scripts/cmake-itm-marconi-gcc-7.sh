f8a6599cbf72a1c02675136e7115bef77b0f7f8e
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2021-12-21 16:14:08 +0100

    Updating script for building on ITM gateway with gcc/7

--- scripts/cmake-itm-marconi-gcc-7.sh
+++ scripts/cmake-itm-marconi-gcc-7.sh
@@ -7,15 +7,16 @@ module ()
 
 module purge
 module load cineca
-module load itmenv
 module unload gnu
 module load gnu/7.3.0
+module load itm-openmpi/4.0.4--gnu--7.3.0
+module load itm-boost/1.61.0--gnu--7.3.0
+module load cmake/3.5.2
 
 export CC=gcc
 export CXX=g++
-export BOOST_ROOT=$HOME/boost_1_62_0
 
 cmake -Bbuild-gcc-7 -H. -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_INSTALL_PREFIX=/gw/swimas/extra/uda/2.3.1/gcc/7.3.0 \
+    -DCMAKE_INSTALL_PREFIX=$UDA_INSTALL \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_PLUGINS=help\;uda $*
