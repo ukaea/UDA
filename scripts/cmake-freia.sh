844771d61560b74aaf3ca08a4532cd5b5b71f019
Stephen Dixon <sdixon@IP-10-215-130-122.vpnzone.ukaea.uk>
2023-05-15 18:55:02 +0100

    fixing freia pyuda build

--- scripts/cmake-freia.sh
+++ scripts/cmake-freia.sh
@@ -7,11 +7,16 @@ module load hdf5-C/1.8.13
 module load gcc/7.3.0
 module load cmake/3.21.0
 
+
 export HDF5_USE_SHLIB=yes
-export BOOST_ROOT=/usr/local/depot/boost-1.60
+export BOOST_ROOT=/usr/local/depot/boost-1-77-0-gcc7.3.0 
+#export CapnProto_DIR=${UDA_HOME}/uda/extlib/capnproto/install/lib64/cmake/CapnProto
 
 CC=gcc CXX=g++ cmake -Bbuild_freia -H. \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=/projects/UDA/uda-install-develop \
     -DBUILD_SHARED_LIBS=ON \
+    -DFETCH_FMT=ON \
+    -DFETCH_SPDLOG=ON \
+    -DENABLE_CAPNP=ON \
     $*
