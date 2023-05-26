08c60546c9f8621f60409a9c22ba7e6cbe9cd200
stephen-dixon <68229525+stephen-dixon@users.noreply.github.com>
2023-05-26 15:53:07 +0100

    Update cmake-freia.sh

--- scripts/cmake-freia.sh
+++ scripts/cmake-freia.sh
@@ -6,16 +6,16 @@ module load java/1.8
 module load hdf5-C/1.8.13
 module load gcc/7.3.0
 module load cmake/3.21.0
+module load capnproto/0.10.4
+module load fmt/10.0.0
+module load spdlog/1.11.0
 
 export HDF5_USE_SHLIB=yes
 export BOOST_ROOT=/usr/local/depot/boost-1-77-0-gcc7.3.0 
-#export CapnProto_DIR=${PWD}/extlib/capnproto/install/lib64/cmake/CapnProto
 
 CC=gcc CXX=g++ cmake -Bbuild_freia -H. \
-    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/projects/UDA/uda-install-develop \
     -DBUILD_SHARED_LIBS=ON \
-    -DFETCH_FMT=ON \
-    -DFETCH_SPDLOG=ON \
     -DENABLE_CAPNP=ON \
     $*
