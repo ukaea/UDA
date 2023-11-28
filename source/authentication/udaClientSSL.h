55a15a60143fc7ccf3f0e666e09a2d5fcaf8eef5
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2023-11-28 20:52:32 +0000

    Adding clang-format config file and running clang-format on authentication files

--- source/authentication/udaClientSSL.h
+++ source/authentication/udaClientSSL.h
@@ -7,26 +7,28 @@
 
 // Create the SSL context and binding to the socket
 // 3 UDA protocol modes: TCP without SSL/TLS, TCP and UDP both with SSL/TLS
-// This set of functions is concerned only with the SSL/TLS protocol (authentication and encryption) - not with establishing socket connections or non SSL TCP transport
+// This set of functions is concerned only with the SSL/TLS protocol (authentication and encryption) - not with
+// establishing socket connections or non SSL TCP transport
 
-// Server host addressed beginng with SSL:// are assumed to be using SSL authentication. The SSL:// prefix is removed to make the connection.
+// Server host addressed beginng with SSL:// are assumed to be using SSL authentication. The SSL:// prefix is removed to
+// make the connection.
 
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <openssl/crypto.h>
-#include <openssl/x509.h>
-#include <openssl/pem.h>
 #include <openssl/asn1.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
 
-#define VERIFY_DEPTH    4
-#define X509STRINGSIZE    256
+#define VERIFY_DEPTH 4
+#define X509STRINGSIZE 256
 
+#include <client/udaClientHostList.h>
 #include <clientserver/export.h>
 #include <clientserver/socketStructs.h>
-#include <client/udaClientHostList.h>
 
 bool getUdaClientSSLDisabled();
-SSL *getUdaClientSSL();
+SSL* getUdaClientSSL();
 void putUdaClientSSLSocket(int s);
 void closeUdaClientSSL();
 void putUdaClientSSLProtocol(int specified);
