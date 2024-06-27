f80d22c17baf77b3f5903b2072e79fc240fb0071
Measures, Jimmy <jimmy.measures@ukaea.uk>
2024-06-27 13:46:46 +0100

    removing spaces at the end of the line in the bytesplugin cpp file to remove this cloud of judgement from other uda devs

--- source/plugins/bytes/bytesPlugin.cpp
+++ source/plugins/bytes/bytesPlugin.cpp
@@ -188,7 +188,7 @@ int do_read(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 
     StringCopy(data_source->path, path, MAXPATH);
     UDA_LOG(UDA_LOG_DEBUG, "expandEnvironmentvariables! \n");
-    expand_environment_variables(data_source->path);    
+    expand_environment_variables(data_source->path);
     
     check_allowed_path(data_source->path);
 
