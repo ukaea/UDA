66220cee76ce5acb011aa066916adae7a2068322
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2024-12-09 11:31:42 +0000

    Adding chunk downloading to BYTES plugin.

--- source/plugins/bytes/readBytesNonOptimally.cpp
+++ source/plugins/bytes/readBytesNonOptimally.cpp
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------
-* IDAM Plugin data Reader to Access Files as a Block of Bytes without Interpretation
+* UDA Plugin data Reader to Access Files as a Block of Bytes without Interpretation
 *
 * Input Arguments:    DATA_SOURCE data_source
 *            SIGNAL_DESC signal_desc
@@ -31,136 +31,88 @@
 #include <clientserver/udaTypes.h>
 #include <clientserver/initStructs.h>
 
-int readBytes(DATA_SOURCE data_source, SIGNAL_DESC signal_desc, DATA_BLOCK* data_block, const ENVIRONMENT* environment)
+#define BYTEFILEDOESNOTEXIST        100001
+#define BYTEFILEATTRIBUTEERROR      100002
+#define BYTEFILEISNOTREGULAR        100003
+#define BYTEFILEOPENERROR           100004
+#define BYTEFILEHEAPERROR           100005
+#define BYTEFILEMD5ERROR            100006
+#define BYTEFILEMD5DIFF             100007
+
+int readBytes(FILE* fh, DATA_BLOCK* data_block, int offset, int max_bytes, const std::string& checksum)
 {
     int err = 0;
 
-    char md5file[2 * MD5_SIZE + 1] = "";
-    char md5check[2 * MD5_SIZE + 1] = "";
+    // Read File (Consider using memory mapped I/O & new type to avoid heap free at end if this is too slow!)
 
-    //----------------------------------------------------------------------
-    // Block Access to External Users
-
-    if (environment->external_user) {
-        err = 999;
-        addIdamError(UDA_CODE_ERROR_TYPE, "readBytes", err, "This Service is Disabled");
-        UDA_LOG(UDA_LOG_DEBUG, "Disabled Service - Requested File: %s \n", data_source.path);
-        return err;
+    int nchar = 0;
+    int buf_offset = 0;
+    int buf_size = 1024 * 1024;
+    if (max_bytes > 0 && max_bytes < buf_size) {
+        buf_size = max_bytes;
     }
 
-    //----------------------------------------------------------------------
-    // Test the filepath
+    data_block->data_n = buf_size;    // 1 less than no. bytes read: Last Byte is an EOF
 
-    if (!IsLegalFilePath(data_source.path)) {
-        err = 999;
-        addIdamError(UDA_CODE_ERROR_TYPE, "readBytes", err, "The directory path has incorrect syntax");
-        UDA_LOG(UDA_LOG_DEBUG, "The directory path has incorrect syntax [%s] \n", data_source.path);
-        return err;
+    if (offset >= 0) {
+        fseek(fh, offset, SEEK_SET);
     }
 
-    //----------------------------------------------------------------------
-    // Data Source Details
-
-    err = 0;
-
-    UDA_LOG(UDA_LOG_DEBUG, "File Name  : %s \n", data_source.path);
-
-    //----------------------------------------------------------------------
-    // File Attributes
-
-    errno = 0;
-
-    //----------------------------------------------------------------------
-    // Open the File as a Binary Stream
-
-    errno = 0;
-    FILE* fh = fopen(data_source.path, "rb");
-
-    int serrno = errno;
-
-    if (fh == nullptr || ferror(fh) || serrno != 0) {
-        err = BYTEFILEOPENERROR;
-        if (serrno != 0) {
-            addIdamError(UDA_SYSTEM_ERROR_TYPE, "readBytes", serrno, "");
-        }
-        addIdamError(UDA_CODE_ERROR_TYPE, "readBytes", err, "Unable to Open the File for Read Access");
-        if (fh != nullptr) {
-            fclose(fh);
-        }
-        return err;
-    }
-
-    //----------------------------------------------------------------------
-    // Error Trap Loop
-
-    do {
-
-        // Read File (Consider using memory mapped I/O & new type to avoid heap free at end if this is too slow!)
-
-        int nchar = 0;
-        int offset = 0;
-        int bufsize = 100 * 1024;
-        data_block->data_n = bufsize;    // 1 less than no. bytes read: Last Byte is an EOF
-
-        char* bp = nullptr;
-        while (!feof(fh)) {
-            if ((bp = (char*)realloc(bp, (size_t)data_block->data_n)) == nullptr) {
-                err = BYTEFILEHEAPERROR;
-                addIdamError(UDA_CODE_ERROR_TYPE, "readBytes", err, "Unable to Allocate Heap Memory for the File");
-                break;
-            }
-            int nread = (int)fread(bp + offset, sizeof(char), (size_t)bufsize, fh);
-            nchar = nchar + nread;
-            offset = nchar;
-            data_block->data_n = nchar + bufsize + 1;
-        }
-
-        if (err != 0) {
+    char* bp = nullptr;
+    while (!feof(fh)) {
+        char* newp = (char*)realloc(bp, (size_t)data_block->data_n);
+        if (newp == nullptr) {
+            free(bp);
+            err = BYTEFILEHEAPERROR;
+            addIdamError(UDA_CODE_ERROR_TYPE, "readBytes", err, "Unable to Allocate Heap Memory for the File");
             break;
         }
+        bp = newp;
+        int n_read = (int)fread(bp + buf_offset, sizeof(char), (size_t)buf_size, fh);
+        nchar += n_read;
+        if (nchar >= max_bytes) {
+            break;
+        }
+        buf_offset = nchar;
+        data_block->data_n = nchar + buf_size + 1;
+    }
 
-        //nchar--;                     // Remove EOF Character from end of Byte Block
-        data_block->data_n = nchar;
-        data_block->data = bp;
+    if (err != 0) {
+        return err;
+    }
 
+    //nchar--;                     // Remove EOF Character from end of Byte Block
+    data_block->data_n = nchar;
+    data_block->data = bp;
+
+    if (checksum == "md5") {
         //----------------------------------------------------------------------
         // MD5 Checksum
 
+        char md5check[MD5_SIZE];
         md5Sum(bp, data_block->data_n, md5check);
-
         strcpy(data_block->data_desc, md5check);    // Pass back the Checksum to the Client
 
         UDA_LOG(UDA_LOG_DEBUG, "File Size          : %d \n", nchar);
-        UDA_LOG(UDA_LOG_DEBUG, "File Checksum      : %s \n", md5file);
         UDA_LOG(UDA_LOG_DEBUG, "Read Checksum      : %s \n", md5check);
-
-        // MD5 Difference?
-
-        //----------------------------------------------------------------------
-        // Fetch Dimensional Data
-
-        data_block->rank = 1;
-        data_block->dims = (DIMS*)malloc(sizeof(DIMS));
-        initDimBlock(data_block->dims);
-
-        data_block->dims[0].data_type = UDA_TYPE_UNSIGNED_INT;
-        data_block->dims[0].dim_n = data_block->data_n;
-        data_block->dims[0].compressed = 1;
-        data_block->dims[0].dim0 = 0.0;
-        data_block->dims[0].diff = 1.0;
-        data_block->dims[0].method = 0;
-
-        data_block->order = -1;        // No Dimensions
-        data_block->data_type = UDA_TYPE_CHAR;
-
-    } while (0);
+    }
 
     //----------------------------------------------------------------------
-    // Housekeeping
+    // Fetch Dimensional Data
 
-//    if (err != 0) {
-//        freeDataBlock(data_block);
-//    }
+    data_block->rank = 1;
+    data_block->dims = (DIMS*)malloc(sizeof(DIMS));
+    initDimBlock(data_block->dims);
+
+    data_block->dims[0].data_type = UDA_TYPE_UNSIGNED_INT;
+    data_block->dims[0].dim_n = data_block->data_n;
+    data_block->dims[0].compressed = 1;
+    data_block->dims[0].dim0 = 0.0;
+    data_block->dims[0].diff = 1.0;
+    data_block->dims[0].method = 0;
+
+    data_block->order = -1;        // No Dimensions
+    data_block->data_type = UDA_TYPE_CHAR;
 
     fclose(fh); // Close the File
 
--- source/plugins/bytes/readBytesNonOptimally.h
+++ source/plugins/bytes/readBytesNonOptimally.h
@@ -1,30 +1,8 @@
-#ifndef UDA_PLUGIN_READBYTESNONOPTIMALLY_H
-#define UDA_PLUGIN_READBYTESNONOPTIMALLY_H
+#pragma once
+
+#include <string>
 
 #include <clientserver/udaStructs.h>
-#include <clientserver/export.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-LIBRARY_API int readBytes(DATA_SOURCE data_source, SIGNAL_DESC signal_desc, DATA_BLOCK *data_block, const ENVIRONMENT* environment);
-
-#ifndef NOBINARYPLUGIN
-
-#define BYTEFILEDOESNOTEXIST     100001
-#define BYTEFILEATTRIBUTEERROR     100002
-#define BYTEFILEISNOTREGULAR     100003
-#define BYTEFILEOPENERROR         100004
-#define BYTEFILEHEAPERROR         100005
-#define BYTEFILEMD5ERROR        100006
-#define BYTEFILEMD5DIFF            100007
-
-#endif
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // UDA_PLUGIN_READBYTESNONOPTIMALLY_H
+int readBytes(FILE* file, DATA_BLOCK* data_block, int offset, int max_bytes, const std::string& checksum);
 
