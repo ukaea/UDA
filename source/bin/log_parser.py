4d5b34fc6763c271fe698df7bb33190ca26efdf0
sdixon <stephen.dixon@ukaea.uk>
2025-06-20 16:27:02 +0100

    updating regex in log_parser.py to match extra mastu examples

--- source/bin/log_parser.py
+++ source/bin/log_parser.py
@@ -4,12 +4,13 @@ import re
 import json
 from collections import namedtuple
 
-RE_STRING = r'(?P<host>\d+\.\d+\.\d+\.\d+) - (?P<uid>\w+)? \[(?P<datetime>[a-zA-Z0-9: ]+)\] \[(?P<req_id>\d+) (?P<signal>.+) (?P<exp_no>\d) (?P<pass>-?\d)    (?P<format>[A-Z]+) (?P<archive>[A-Z]+)  \] (?P<err_code>\d+) (?P<db_size>\d+) \[(?P<err_msg>.*)\] (?P<elapsed_time>\d+\.\d+) (?P<client_ver>\d+) (?P<server_ver>\d+) \[(?P<client_pid>\d+) (?P<server_pid>\d+)\] \[\]'
+RE_STRING = r'(?P<host>\d+\.\d+\.\d+\.\d+) - (?P<uid>[\w\d_\-]+)? \[(?P<datetime>[a-zA-Z0-9: ]+)\] \[(?P<req_id>\d+) (?P<signal>.+) (?P<exp_no>\d+) (?P<pass>-?\d+)  (?P<path>[\w\d\-_\.\/+]+)? (?P<file>[\w\d\-_\.\/+]+)? (?P<format>[a-zA-Z_]+)? (?P<archive>[a-zA-Z_]+)? (?P<device>[a-zA-Z_]+)? \] (?P<err_code>\d+) (?P<db_size>\d+) \[(?P<err_msg>.*)\] (?P<elapsed_time>\d+(?:\.\d*(?:e[+\-]?\d+)?)?) (?P<client_ver>\d+) (?P<server_ver>\d+) \[(?P<client_pid>\d+) (?P<server_pid>\d+)\] \[\]'
 
 log_regex = re.compile(RE_STRING)
 
 LogLine = namedtuple('LogLine',
-    ['host', 'uid', 'datetime', 'req_id', 'signal', 'exp_no', 'pass_no', 'format', 'archive', 'err_code', 'db_size', 'err_msg',
+    ['host', 'uid', 'datetime', 'req_id', 'signal', 'exp_no', 'pass_no', 'path',
+     'file', 'format', 'archive', 'device', 'err_code', 'db_size', 'err_msg',
      'elapsed_time', 'client_ver', 'server_ver', 'client_pid', 'server_pid'])
 
 
@@ -20,6 +21,7 @@ def parse(logfile, outfile):
             match = log_regex.match(line)
             if not match:
                 print(f'failed to parse log line {i + 1}:\n{line}', file=sys.stderr)
+                continue
             groups = match.groups()
             log_line = LogLine(*groups)
             lines.append(log_line._asdict())
