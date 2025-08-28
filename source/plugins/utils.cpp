02db8e09d9d947e9c4f73be4a71128eed00b25c6
Measures, Jimmy <jimmy.measures@ukaea.uk>
2025-08-28 14:05:59 +0100

    Looks like some suggestions were rouses

--- source/plugins/utils.cpp
+++ source/plugins/utils.cpp
@@ -36,7 +36,7 @@ int check_allowed_path(const char* expandedPath) {
     const char* env_str = std::getenv("UDA_ALLOWED_PATHS");
 
     std::vector<std::string> allowed_paths;
-    if (env_str != nullptr && env_str) {
+    if (env_str && !std::string(env_str).empty()) {
         // Checking if environment variable exists before using it
         boost::split(allowed_paths, env_str, boost::is_any_of(";"));
         boost::split(allowed_paths, env_str, boost::is_any_of(";"));
